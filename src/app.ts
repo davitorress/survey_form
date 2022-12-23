c7dc76274562013314fa5a50aaad5b9cf04c0a35
davitorress <dev.davitorres@gmail.com>
2022-12-23 15:50:25 -0300

    feat: submit events and get local storage data

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 // Navigation Events
 class NavigationEvent {
-	static instance: NavigationEvent;
+	private static instance: NavigationEvent;
 
 	paths: string[] = [
 		"/public/",
@@ -53,7 +53,6 @@ const survey = JSON.parse(localStorage.getItem("survey")!) || {
 };
 
 // Clean LocalStorage
-console.log(document.location.pathname);
 if (document.location.pathname === "/public/") {
 	localStorage.setItem(
 		"survey",
@@ -73,20 +72,24 @@ if (document.location.pathname === "/public/") {
 // Select input
 const selectInput = document.querySelector("select")! as HTMLSelectElement;
 if (selectInput) {
-	selectInput.addEventListener("change", () => {
-		survey[selectInput.id] = selectInput.value;
-		localStorage.setItem("survey", JSON.stringify(survey));
-	});
+	if (survey[selectInput.id]) selectInput.value = survey[selectInput.id];
+
+	// selectInput.addEventListener("change", () => {
+	// 	survey[selectInput.id] = selectInput.value;
+	// 	localStorage.setItem("survey", JSON.stringify(survey));
+	// });
 }
 
 // Radio input
 const radioInput: HTMLInputElement[] = document.querySelectorAll(".radio")! as any;
 if (radioInput) {
 	radioInput.forEach((radio: HTMLInputElement) => {
-		radio.addEventListener("click", () => {
-			survey[radio.name] = radio.value;
-			localStorage.setItem("survey", JSON.stringify(survey));
-		});
+		if (survey[radio.name] !== "" && radio.value === survey[radio.name]) radio.checked = true;
+
+		// radio.addEventListener("click", () => {
+		// 	survey[radio.name] = radio.value;
+		// 	localStorage.setItem("survey", JSON.stringify(survey));
+		// });
 	});
 }
 
@@ -94,54 +97,122 @@ if (radioInput) {
 const checkInput: HTMLInputElement[] = document.querySelectorAll(".check")! as any;
 const checkValues: string[] = [];
 if (checkInput) {
-	checkInput.forEach((check: HTMLInputElement) => {
-		check.addEventListener("change", () => {
-			if (!checkValues.includes(check.value)) checkValues.push(check.value);
-			else {
-				const position = checkValues.findIndex((value: string) => value === check.value);
-				checkValues.splice(position, 1);
-			}
+	const checkedArray = survey.resources_training.split(",");
 
-			survey[check.name] = `${checkValues}`;
-			localStorage.setItem("survey", JSON.stringify(survey));
-		});
+	checkInput.forEach((check: HTMLInputElement) => {
+		if (checkedArray.includes(check.value)) check.checked = true;
+
+		// check.addEventListener("change", () => {
+		// 	if (!checkValues.includes(check.value)) checkValues.push(check.value);
+		// 	else {
+		// 		const position = checkValues.findIndex((value: string) => value === check.value);
+		// 		checkValues.splice(position, 1);
+		// 	}
+		// 	survey[check.name] = `${checkValues}`;
+		// 	localStorage.setItem("survey", JSON.stringify(survey));
+		// });
 	});
 }
 
 // TextArea
-const textAreaInput: HTMLTextAreaElement = document.querySelector("textarea")! as HTMLTextAreaElement;
+const textAreaInput = document.querySelector("textarea")! as HTMLTextAreaElement;
 if (textAreaInput) {
-	textAreaInput.addEventListener("keydown", (e: Event) => {
-		survey[textAreaInput.id] = textAreaInput.value;
-		localStorage.setItem("survey", JSON.stringify(survey));
-	});
+	if (survey[textAreaInput.id] !== "") textAreaInput.value = survey[textAreaInput.id];
+
+	// textAreaInput.addEventListener("keydown", (e: Event) => {
+	// 	survey[textAreaInput.id] = textAreaInput.value;
+	// 	localStorage.setItem("survey", JSON.stringify(survey));
+	// });
 }
 
 // Name Input
-const nameInput: HTMLInputElement = document.querySelector("#full_name")! as HTMLInputElement;
+const nameInput = document.querySelector("#full_name")! as HTMLInputElement;
 if (nameInput) {
-	nameInput.addEventListener("keydown", (e: Event) => {
-		survey[nameInput.id] = nameInput.value;
-		localStorage.setItem("survey", JSON.stringify(survey));
-	});
+	if (survey[nameInput.id] !== "") nameInput.value = survey[nameInput.id];
+
+	// nameInput.addEventListener("keydown", (e: Event) => {
+	// 	survey[nameInput.id] = nameInput.value;
+	// 	localStorage.setItem("survey", JSON.stringify(survey));
+	// });
 }
 
 // Email Input
-const emailInput: HTMLInputElement = document.querySelector("#email")! as HTMLInputElement;
+const emailInput = document.querySelector("#email")! as HTMLInputElement;
 if (emailInput) {
-	emailInput.addEventListener("keydown", (e: Event) => {
-		survey[emailInput.id] = emailInput.value;
-		localStorage.setItem("survey", JSON.stringify(survey));
-	});
+	// emailInput.addEventListener("keydown", (e: Event) => {
+	// 	survey[emailInput.id] = emailInput.value;
+	// 	localStorage.setItem("survey", JSON.stringify(survey));
+	// });
 }
 
 // Age Input
-const ageInput: HTMLInputElement = document.querySelector("#age")! as HTMLInputElement;
+const ageInput = document.querySelector("#age")! as HTMLInputElement;
 if (ageInput) {
-	["click", "keydown"].forEach((eventType: string) =>
-		ageInput.addEventListener(eventType, (e: Event) => {
-			survey[ageInput.id] = +ageInput.value;
+	// ["click", "keydown"].forEach((eventType: string) =>
+	// 	ageInput.addEventListener(eventType, (e: Event) => {
+	// 		survey[ageInput.id] = +ageInput.value;
+	// 		localStorage.setItem("survey", JSON.stringify(survey));
+	// 	})
+	// );
+}
+
+// Submit
+type InputsElements = HTMLSelectElement | HTMLInputElement | HTMLTextAreaElement;
+
+class Submit {
+	private static instance: Submit;
+
+	constructor() {}
+
+	static getInstance() {
+		if (this.instance) this.instance;
+		this.instance = new Submit();
+		return this.instance;
+	}
+
+	basic(element: InputsElements) {
+		survey[element.id] = element.value;
+		localStorage.setItem("survey", JSON.stringify(survey));
+	}
+
+	number(element: InputsElements) {
+		survey[element.id] = +element.value;
+		localStorage.setItem("survey", JSON.stringify(survey));
+	}
+
+	radio(element: InputsElements) {
+		survey[element.name] = element.value;
+		localStorage.setItem("survey", JSON.stringify(survey));
+	}
+
+	check(element: HTMLInputElement[]) {
+		const checkValues: string[] = [];
+		element.forEach((check: HTMLInputElement) => {
+			if (!checkValues.includes(check.value) && check.checked) checkValues.push(check.value);
+			survey[check.name] = `${checkValues}`;
 			localStorage.setItem("survey", JSON.stringify(survey));
-		})
-	);
+		});
+	}
+}
+const submit = Submit.getInstance();
+
+// Next button
+const nextBtn = document.querySelector(".nextBtn")! as HTMLButtonElement;
+if (nextBtn) {
+	nextBtn.addEventListener("click", (e: Event) => {
+		e.preventDefault();
+
+		if (selectInput) submit.basic(selectInput);
+		if (textAreaInput) submit.basic(textAreaInput);
+		if (nameInput) submit.basic(nameInput);
+		if (emailInput) submit.basic(emailInput);
+		if (ageInput) submit.number(ageInput);
+		if (checkInput) submit.check(checkInput);
+		if (radioInput) {
+			radioInput.forEach((radio: HTMLInputElement) => {
+				if (radio.checked) submit.radio(radio);
+			});
+		}
+	});
+	nav.navigate(nextBtn, window.location.pathname);
 }
