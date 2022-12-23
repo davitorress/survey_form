8380c40834d04cd2dba0b23801dd38bad158d3cb
davitorress <dev.davitorres@gmail.com>
2022-12-23 11:50:09 -0300

    feat: data save on local storage

--- src/app.ts
+++ src/app.ts
@@ -40,3 +40,96 @@ if (backBtn) nav.navigate(backBtn, window.location.pathname, true);
 // Skip button
 const skipBtn = document.querySelector(".skipBtn")! as HTMLButtonElement;
 if (skipBtn) nav.navigate(skipBtn, window.location.pathname);
+
+const survey = JSON.parse(localStorage.getItem("survey")!) || {
+	account_status: "",
+	financial_market: "",
+	investment_type: "",
+	resources_training: "",
+	loss_details: "",
+	full_name: "",
+	email: "",
+	age: 0,
+};
+
+// Clean LocalStorage
+console.log(document.location.pathname);
+if (document.location.pathname === "/public/") {
+	localStorage.setItem("survey", JSON.stringify(survey));
+}
+
+// Select input
+const selectInput = document.querySelector("select")! as HTMLSelectElement;
+if (selectInput) {
+	selectInput.addEventListener("change", () => {
+		survey[selectInput.id] = selectInput.value;
+		localStorage.setItem("survey", JSON.stringify(survey));
+	});
+}
+
+// Radio input
+const radioInput: HTMLInputElement[] = document.querySelectorAll(".radio")! as any;
+if (radioInput) {
+	radioInput.forEach((radio: HTMLInputElement) => {
+		radio.addEventListener("click", () => {
+			survey[radio.name] = radio.value;
+			localStorage.setItem("survey", JSON.stringify(survey));
+		});
+	});
+}
+
+// Checkbox
+const checkInput: HTMLInputElement[] = document.querySelectorAll(".check")! as any;
+const checkValues: string[] = [];
+if (checkInput) {
+	checkInput.forEach((check: HTMLInputElement) => {
+		check.addEventListener("change", () => {
+			if (!checkValues.includes(check.value)) checkValues.push(check.value);
+			else {
+				const position = checkValues.findIndex((value: string) => value === check.value);
+				checkValues.splice(position, 1);
+			}
+
+			survey[check.name] = `${checkValues}`;
+			localStorage.setItem("survey", JSON.stringify(survey));
+		});
+	});
+}
+
+// TextArea
+const textAreaInput: HTMLTextAreaElement = document.querySelector("textarea")! as HTMLTextAreaElement;
+if (textAreaInput) {
+	textAreaInput.addEventListener("keydown", (e: Event) => {
+		survey[textAreaInput.id] = textAreaInput.value;
+		localStorage.setItem("survey", JSON.stringify(survey));
+	});
+}
+
+// Name Input
+const nameInput: HTMLInputElement = document.querySelector("#full_name")! as HTMLInputElement;
+if (nameInput) {
+	nameInput.addEventListener("keydown", (e: Event) => {
+		survey[nameInput.id] = nameInput.value;
+		localStorage.setItem("survey", JSON.stringify(survey));
+	});
+}
+
+// Email Input
+const emailInput: HTMLInputElement = document.querySelector("#email")! as HTMLInputElement;
+if (emailInput) {
+	emailInput.addEventListener("keydown", (e: Event) => {
+		survey[emailInput.id] = emailInput.value;
+		localStorage.setItem("survey", JSON.stringify(survey));
+	});
+}
+
+// Age Input
+const ageInput: HTMLInputElement = document.querySelector("#age")! as HTMLInputElement;
+if (ageInput) {
+	["click", "keydown"].forEach((eventType: string) =>
+		ageInput.addEventListener(eventType, (e: Event) => {
+			survey[ageInput.id] = +ageInput.value;
+			localStorage.setItem("survey", JSON.stringify(survey));
+		})
+	);
+}
